288b326b663afcaa87f46b0c69d86bd3e5566341
joaquin <joaquinleonquero00@gmail.com>
2022-04-24 16:15:04 -0500

    Se realiza seccion 04 introduccion a Angular

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { ConntadorComponent } from './contador/contador.componet';
+
+
+
+
+@NgModule({
+    declarations: [ConntadorComponent],
+
+    exports: [ConntadorComponent]
+
+})
+
+export class ContadorModule{}
